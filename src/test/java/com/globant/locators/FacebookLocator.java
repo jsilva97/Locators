45f0fb3d09b049f979cdfac94a175e78e1a50f8c
Juan Silva <juanfelipe.silva@globant.com>
2022-04-21 17:46:26 -0500

    Locators Practice

---
+++ src/test/java/com/globant/locators/FacebookLocator.java
@@ -0,0 +1,11 @@
+package com.globant.locators;
+
+public class FacebookLocator {
+    //Por cuestiones del taller daré los valores donde declaró las variables.
+    // Ya se que no es buena pratica hacerlo así, se deberia dar el valor en el constructor.
+    private String LogInButton = "._6ltg button";
+    private String ArrowMenu = "div[role='banner'] div:nth-child(4) div[aria-label*='Controles']>span>div";
+    private String LogOutButton = ".b20td4e0 div:nth-child(4)";
+    private String MyProfile = "div[role='dialog']>div>div>div>div>div>div>div>div>div>div>a";
+    private String MyFriends = "a[aria-label='Facebook']";
+}
---
+++ src/test/java/com/globant/locators/LocatorHeroku.java
@@ -0,0 +1,18 @@
+package com.globant.locators;
+
+public class LocatorHeroku {
+
+    //Por cuestiones del taller daré los valores donde declaró las variables.
+    // Ya se que no es buena pratica hacerlo así, se deberia dar el valor en el constructor.
+    private String elemento1 = ".container p:nth-child(3)";
+    private String elemento2 = "address a:nth-child(1)[css='1']";
+    private String elemento3 = "ul li:nth-child(3)";
+    private String elemento4 = "menu li:first-child";
+    private String elemento5 = "dir li:last-child";
+    private String elemento6 = "label[for='but'] button";
+    private String elemento7 = "label[for='f01'] input";
+    private String elemento8 = "#f11 option:last-child";
+    private String elemento9 = "tbody td[xpath='1']";
+    private String elemento10 = "p[xpath='1']";
+    private String elemento11 = "h3[xpath='1']";
+}
